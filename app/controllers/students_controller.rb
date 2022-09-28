ff65d2904c1c60963a5a955e9db4d45b399a2dca
SallyMulupi <mulupisally2gmail.com>
2022-09-29 00:05:08 +0300

    commit tree 5626160967b7c03e3af6f1ca424cc2b2de5307d7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,12 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
-
+  def grades
+    students = Student.all.sort_by{|s| -s.grade}
+    render json: students
+  end
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get '/students', to: 'students#index'
-  get '/students/grades', to: 'students#grades'
-  get '/students/highest-grade', to: 'students#highest_grade'
+  get 'students', to: 'students#index'
+  get 'students/grades', to: 'students#grades'
+  get '/students/:id', to: 'students#show'
+  # get '/students/1', to: "students#first"
+  # get '/students/2', to: "students#second"
+ 
+  # get '/students/highest-grade', to: 'students#highest_grade'
+
+
+
 end
